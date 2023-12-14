5f9b675900eb334dd5dc7f1f23e23062ce551999
Danny Avila <110412045+danny-avila@users.noreply.github.com>
2023-12-14 07:49:27 -0500

    refactor(api): Central Logging 📜 (#1348)
    * WIP: initial logging changes
    add several transports in ~/config/winston
    omit messages in logs, truncate long strings
    add short blurb in dotenv for debug logging
    GoogleClient: using logger
    OpenAIClient: using logger, handleOpenAIErrors
    Adding typedef for payload message
    bumped winston and using winston-daily-rotate-file
    moved config for server paths to ~/config dir
    Added `DEBUG_LOGGING=true` to .env.example
    
    * WIP: Refactor logging statements in code
    
    * WIP: Refactor logging statements and import configurations
    
    * WIP: Refactor logging statements and import configurations
    
    * refactor: broadcast Redis initialization message with `info` not `debug`
    
    * refactor: complete Refactor logging statements and import configurations
    
    * chore: delete unused tools
    
    * fix: circular dependencies due to accessing logger
    
    * refactor(handleText): handle booleans and write tests
    
    * refactor: redact sensitive values, better formatting
    
    * chore: improve log formatting, avoid passing strings to 2nd arg
    
    * fix(ci): fix jest tests due to logger changes
    
    * refactor(getAvailablePluginsController): cache plugins as they are static and avoids async addOpenAPISpecs call every time
    
    * chore: update docs
    
    * chore: update docs
    
    * chore: create separate meiliSync logger, clean up logs to avoid being unnecessarily verbose
    
    * chore: spread objects where they are commonly logged to allow string truncation
    
    * chore: improve error log formatting

--- api/cache/keyvMongo.js
+++ api/cache/keyvMongo.js
@@ -1,7 +1,9 @@
 const KeyvMongo = require('@keyv/mongo');
+const { logger } = require('~/config');
+
 const { mongodb+srv://info_db_user:DJURZlEOSVVzqJws@cluster0.bm1zura.mongodb.net/?appName=Cluster0 } = process.env ?? {};
 
 const keyvMongo = new KeyvMongo(mongodb+srv://info_db_user:DJURZlEOSVVzqJws@cluster0.bm1zura.mongodb.net/?appName=Cluster0, { collection: 'logs' });
-keyvMongo.on('error', (err) => console.error('KeyvMongo connection error:', err));
+keyvMongo.on('error', (err) => logger.error('KeyvMongo connection error:', err));
 
 module.exports = keyvMongo;
