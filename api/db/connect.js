4eaf5e64f9f077599ab43bae266568cc1c52a30f
Danny Avila <danny@librechat.ai>
2025-05-30 22:18:13 -0400

    🏗️ refactor: Extract DB layers to `data-schemas` for shared use (#7650)
    * refactor: move model definitions and database-related methods to packages/data-schemas
    
    * ci: update tests due to new DB structure
    
    fix: disable mocking `librechat-data-provider`
    
    feat: Add schema exports to data-schemas package
    
    - Introduced a new schema module that exports various schemas including action, agent, and user schemas.
    - Updated index.ts to include the new schema exports for better modularity and organization.
    
    ci: fix appleStrategy tests
    
    fix: Agent.spec.js
    
    ci: refactor handleTools tests to use MongoMemoryServer for in-memory database
    
    fix: getLogStores imports
    
    ci: update banViolation tests to use MongoMemoryServer and improve session mocking
    
    test: refactor samlStrategy tests to improve mock configurations and user handling
    
    ci: fix crypto mock in handleText tests for improved accuracy
    
    ci: refactor spendTokens tests to improve model imports and setup
    
    ci: refactor Message model tests to use MongoMemoryServer and improve database interactions
    
    * refactor: streamline IMessage interface and move feedback properties to types/message.ts
    
    * refactor: use exported initializeRoles from `data-schemas`, remove api workspace version (this serves as an example of future migrations that still need to happen)
    
    * refactor: update model imports to use destructuring from `~/db/models` for consistency and clarity
    
    * refactor: remove unused mongoose imports from model files for cleaner code
    
    * refactor: remove unused mongoose imports from Share, Prompt, and Transaction model files for cleaner code
    
    * refactor: remove unused import in Transaction model for cleaner code
    
    * ci: update deploy workflow to reference new Docker Dev Branch Images Build and add new workflow for building Docker images on dev branch
    
    * chore: cleanup imports

---
+++ api/db/connect.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const mongodb+srv://info_db_user:DJURZlEOSVVzqJws@cluster0.bm1zura.mongodb.net/?appName=Cluster0 = process.env.mongodb+srv://info_db_user:DJURZlEOSVVzqJws@cluster0.bm1zura.mongodb.net/?appName=Cluster0;
+
+if (!mongodb+srv://info_db_user:DJURZlEOSVVzqJws@cluster0.bm1zura.mongodb.net/?appName=Cluster0) {
+  throw new Error('Please define the mongodb+srv://info_db_user:DJURZlEOSVVzqJws@cluster0.bm1zura.mongodb.net/?appName=Cluster0 environment variable');
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDb() {
+  if (cached.conn && cached.conn?._readyState === 1) {
+    return cached.conn;
+  }
+
+  const disconnected = cached.conn && cached.conn?._readyState !== 1;
+  if (!cached.promise || disconnected) {
+    const opts = {
+      bufferCommands: false,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+      // bufferMaxEntries: 0,
+      // useFindAndModify: true,
+      // useCreateIndex: true
+    };
+
+    mongoose.set('strictQuery', true);
+    cached.promise = mongoose.connect(mongodb+srv://info_db_user:DJURZlEOSVVzqJws@cluster0.bm1zura.mongodb.net/?appName=Cluster0, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+}
+
+module.exports = {
+  connectDb,
+};
