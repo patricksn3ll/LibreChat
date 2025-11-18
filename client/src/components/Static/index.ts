7da00139934907b8e53933d0c8a0f6a8eb1bd459
patricksn3ll <sn3ll@hotmail.com>
2025-11-18 12:27:49 -0500

    Missing import

--- client/src/components/Static/index.ts
+++ client/src/components/Static/index.ts
@@ -3,3 +3,4 @@ export { default as PrivacyPolicy } from './PrivacyPolicy';
 export { default as TermsAndServices } from './TermsAndServices';
 export { default as Plans } from './Plans';
 export { default as LandingPageWrapper } from './LandingPageWrapper';
+export { default as Landing } from './Landing';
