9c5b70c489f0841c68d02c0f707c517298e229d1
patricksn3ll <sn3ll@hotmail.com>
2025-11-18 06:31:32 -0500

    Adds some white images

--- client/src/components/Static/LandingPageWrapper.tsx
+++ client/src/components/Static/LandingPageWrapper.tsx
@@ -1,5 +1,5 @@
 import { AuthContextProvider } from '~/hooks/AuthContext';
-import { Landing } from './Landing';
+import  Landing  from './Landing';
 
 export default function LandingPageWrapper() {
   return (
