c427252b92de44819d96e6ccf8db66430385a8c4
patricksn3ll <sn3ll@hotmail.com>
2025-11-19 04:11:11 -0500

    Fix for routing issues with landing

--- client/src/routes/Layouts/LandingLayout.tsx
+++ client/src/routes/Layouts/LandingLayout.tsx
@@ -3,6 +3,7 @@ import { useRecoilState } from 'recoil';
 import { useAuthContext } from '~/hooks/AuthContext';
 import StartupLayout from './Startup';
 import store from '~/store';
+import LandingWrapper from './LandingWrapper';
 
 export default function LandingLayout() {
   const { isAuthenticated } = useAuthContext();
@@ -19,5 +20,5 @@ export default function LandingLayout() {
       clearTimeout(timeout);
     };
   }, [queriesEnabled, setQueriesEnabled]);
-  return <StartupLayout isAuthenticated={isAuthenticated} />;
+  return <LandingWrapper isAuthenticated={isAuthenticated} />;
 }
