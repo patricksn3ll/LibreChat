02c6f1f0d309e029a07fa25237f72c4cd210fa06
patricksn3ll <sn3ll@hotmail.com>
2025-11-19 08:37:13 -0500

    Fix compile issue

--- client/src/components/Static/index.ts
+++ client/src/components/Static/index.ts
@@ -3,4 +3,3 @@ export { default as PrivacyPolicy } from './PrivacyPolicy';
 export { default as TermsAndServices } from './TermsAndServices';
 export { default as Plans } from './Plans';
 export { default as LandingPageWrapper } from './LandingPageWrapper';
-export { default as Landing } from './Landing';
