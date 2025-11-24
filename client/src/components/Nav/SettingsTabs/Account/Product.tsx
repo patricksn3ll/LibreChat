be788dd5f085db6cd6e1abcd056c82fd608ffd35
patricksn3ll <sn3ll@hotmail.com>
2025-11-24 18:37:30 -0500

    Revert back to buttons

--- client/src/components/Nav/SettingsTabs/Account/Product.tsx
+++ client/src/components/Nav/SettingsTabs/Account/Product.tsx
@@ -1,16 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useSearchParams, useParams, useNavigate } from 'react-router-dom';
 import { useAuthContext } from '~/hooks';
 import { PRODUCTS } from './products';
 import { useCreateStripeCheckoutSession } from './useCreateStripeCheckoutSession';
 import { useCancelSubscription } from './useCancelSubscription';
-import { Dropdown, Label } from '@librechat/client';
 
 function Product({ open, onOpenChange }: TDialogProps) {
   const { user, token } = useAuthContext();
   //const { mutate: createCheckoutSession, isLoading: purchasing, variables: subscribingPlan } = useCreateStripeCheckoutSession();
   const [purchasing, setPurchasing] = useState('');
-  const [selectedProductId, setSelectedProductId] = useState(null);
   const [billingLoading, setBillingLoading] = useState(false);
   const navigate = useNavigate();
 
@@ -24,18 +22,6 @@ function Product({ open, onOpenChange }: TDialogProps) {
   const handleCancel = () => {
     setShowConfirm(true);
   };
-
-  async function onChange(value: string) {
-    if (selectedProductId === value) {
-      return;
-    }
-
-    setSelectedProductId(productOptions[1].id);
-
-    // Get the price for the selected product id
-    const price = PRODUCTS.find(product => product.id === value)?.price;
-    await handlePurchaseClick(value, price);
-  }
 
   async function handlePurchaseClick(priceId, amount) {
     try {
@@ -91,39 +77,10 @@ function Product({ open, onOpenChange }: TDialogProps) {
       .finally(() => setBillingLoading(false));
   };
 
-  useEffect(() => {
-    setSelectedProductId(PRODUCTS[1].id);
-  }, []); 
-
-  const productOptions = [
-    { value: PRODUCTS[0].id, label: PRODUCTS[0].name },
-    { value: PRODUCTS[1].id, label: PRODUCTS[1].name },
-    { value: PRODUCTS[2].id, label: PRODUCTS[2].name },
-    { value: PRODUCTS[3].id, label: PRODUCTS[3].name }
-  ];
-
   return (
-    <>
-      <div className="flex items-center justify-between">
-        <div className="flex items-center space-x-2">
-          <Label className="font-light">Purchase additional credits safley and securly. Your credits ever expire.</Label>
-        </div>
-
-        <span className="text-sm font-medium text-gray-800 dark:text-gray-200" role="note">
-          <Dropdown
-            value={selectedProductId}
-            onChange={onChange}
-            options={productOptions}
-            sizeClasses="w-full"
-            className="z-50"
-          />        
-        </span>
-      </div>
-      <div className="flex justify-center mt-4">
-        <img src="assets/stripe-security-badge.png" alt="Stripe Security Badge" className="w-full max-w-xs" />
-      </div>
-
-       {/* {PRODUCTS.map((p) => (
+    <div className="flex flex-col gap-3 p-1 text-sm text-text-primary">  
+      <div className="mt-2">
+        {PRODUCTS.map((p) => (
           <div
             key={p.id}
             className="flex flex-col md:flex-row px-4 py-2 md:items-center md:gap-4 md:col-span-2"
@@ -138,9 +95,12 @@ function Product({ open, onOpenChange }: TDialogProps) {
               </span>
             </button>
           </div>
-        ))}         */}
-
-    </>
+        ))}        
+      </div>
+      <div className="flex justify-center mt-4">
+        <img src="assets/stripe-security-badge.png" alt="Stripe Security Badge" className="w-full max-w-xs" />
+      </div>
+    </div>        
   );
 }
 
