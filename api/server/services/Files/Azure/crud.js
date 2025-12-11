0a881e4df67c3b050de76a0170c116fbd2947d23
patricksn3ll <sn3ll@hotmail.com>
2025-12-11 05:09:23 -0500

    Testing

--- api/server/services/Files/Azure/crud.js
+++ api/server/services/Files/Azure/crud.js
@@ -31,7 +31,7 @@ async function saveBufferToAzure({
 }) {
   try {
     const containerClient = await getAzureContainerClient(containerName);
-    logger.error('const containerClient = await getAzureContainerClient(containerName)');    
+    logger.error('const containerClient = await getAzureContainerClient(containerName)', containerName);    
     const access = AZURE_STORAGE_PUBLIC_ACCESS?.toLowerCase() === 'true' ? 'blob' : undefined;
     // Create the container if it doesn't exist. This is done per operation.
     await containerClient.createIfNotExists({ access });
